9390b492ad11450756d23cf62ef95332a05e6562
Sam Kortekaas <sam.kortekaas@jetbrains.com>
2021-12-11 14:44:17 +0100

    Added repl and file input

--- common.h
+++ common.h
@@ -8,6 +8,8 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define DEBUG_TRACE_EXECUTION
 
--- main.c
+++ main.c
@@ -1,38 +1,72 @@
-#include <stdlib.h>
 #include "chunk.h"
-#include "debug.h"
 #include "vm.h"
 
-int main(void) {
+void repl();
+void runFile(const char* path);
+char* readFile(const char* path);
+
+int main(int argc, const char** argv) {
 	initVM();
-	Chunk chunk;
-	initChunk(&chunk);
 
-	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-
-	constant = addConstant(&chunk, 3.4);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-
-	writeChunk(&chunk, OP_ADD, 123);
-
-	constant = addConstant(&chunk, 5.6);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-
-	writeChunk(&chunk, OP_DIVIDE, 123);
-
-	writeChunk(&chunk, OP_NEGATE, 123);
-	writeChunk(&chunk, OP_RETURN, 123);
-
-	interpret(&chunk);
+	if (argc == 1) {
+		repl();
+	} else if (argc == 2) {
+		runFile(argv[1]);
+	}
+	else {
+		fprintf(stderr, "Usage; clox [path]\n");
+	}
 
 	freeVM();
-	freeChunk(&chunk);
-
-//	system("leaks crafting_interpreters");
 	return (0);
 }
 
+void repl() {
+	char line[1024];
+	while (true) {
+		printf("> ");
+
+		if (!fgets(line, sizeof(line), stdin)) {
+			printf("\n");
+			break;
+		}
+
+		interpret(line);
+	}
+}
+
+void runFile(const char* path) {
+	char* source = readFile(path);
+	InterpretResult result = interpret(source);
+	free(source);
+
+	if (result == INTERPRET_COMPILE_ERROR) exit(65);
+	if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
+char* readFile(const char* path) {
+	FILE* file = fopen(path, "rb");
+	if (file == NULL) {
+		fprintf(stderr, "Could not open file \"%s\"\n", path);
+		exit(74);
+	}
+
+	fseek(file, 0L, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+
+	char* buffer = malloc(fileSize + 1);
+	if (buffer == NULL) {
+		fprintf(stderr, "Could not allocate memory for buffer\n");
+		exit(74);
+	}
+	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+	if (bytesRead < fileSize) {
+		fprintf(stderr, "Could not read file\n");
+		exit(74);
+	}
+	buffer[bytesRead] = '\0';
+
+	fclose(file);
+	return buffer;
+}
